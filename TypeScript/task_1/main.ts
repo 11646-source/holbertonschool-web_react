52439c289bb566711d892dd7a5fb957e1c3e8cf3
Ryan364412 <bundhoor50@gmail.com>
2026-05-29 10:06:32 +0400

    task

---
+++ TypeScript/task_1/main.ts
@@ -0,0 +1,11 @@
+// main.ts
+
+interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee:boolean
+	yearsOfExperience: number;
+	location: string;
+
+	[key: string]: any;
+}
